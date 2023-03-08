8aacc275d1b9f0dcd90d32d906b6848da31ad167
Damar Aji Pangestu <112254986+Damaruta@users.noreply.github.com>
2023-03-08 19:35:17 +0700

    Update vektor.r

--- Vektor/vektor.r
+++ Vektor/vektor.r
@@ -1,1 +1,12 @@
+# 1
+v <- c(2, 3, -1, 2)
+w <- c(-2, 3, 3, -1)
 
+norm_v <- norm(v)
+norm_v
+
+distance(v, w) = ||v - w||
+               = sqrt((v1 - w1)^2 + (v2 - w2)^2 + ... + (vn - wn)^2)
+
+distance_vw <- norm(v - w)
+distance_vw
